d45b627e962fa8ff732b0ede1b6045d5224730e2
Your Real Name Here <Your Email Address Here>
2017-02-03 14:01:17 +0530

    First Commit

---
+++ src/main/java/com/noblemktkyc/model/User.java
@@ -0,0 +1,58 @@
+package com.noblemktkyc.model;
+
+import java.io.Serializable;
+
+import com.box.sdk.BoxFolder;
+
+/**
+ * @author Silicus Technologies, 2016
+ * 
+ */
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String directoryPath;
+	private BoxFolder boxFolder;
+	private String status;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getDirectoryPath() {
+		return directoryPath;
+	}
+
+	public void setDirectoryPath(String directoryPath) {
+		this.directoryPath = directoryPath;
+	}
+
+	public BoxFolder getBoxFolder() {
+		return boxFolder;
+
+	}
+
+	public void setBoxFolder(BoxFolder boxFolder) {
+		this.boxFolder = boxFolder;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", directoryPath=" + directoryPath + ", boxFolder=" + boxFolder
+				+ ", status=" + status + "]";
+	}
+
+}
